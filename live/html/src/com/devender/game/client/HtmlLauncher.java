5eef7d68c1e2352152507e948ef635f0549a60d3
devender <devender@adap.tv>
2015-02-09 12:10:14 +0530

    Initial commit

---
+++ live/html/src/com/devender/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.devender.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.devender.game.CatchRain;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(800, 480);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new CatchRain();
+        }
+}
